7821033139ef8e94f439a6704b0e3cea4a11cf41
Terzidest <terzidest@gmail.com>
2025-06-10 20:37:50 +0300

    removed phone from contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -30,22 +30,6 @@ const Contact = () => {
                 </div>
               </div>
               
-              <div className="bg-white rounded-xl shadow-md p-6 flex items-start">
-                <div className="w-10 h-10 rounded-full bg-blue-100 flex items-center justify-center mr-4 flex-shrink-0">
-                  <svg className="w-5 h-5 text-blue-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-                  </svg>
-                </div>
-                <div>
-                  <h3 className="font-bold text-gray-800 mb-1">Phone</h3>
-                  <a 
-                    href="tel:+306940970111" 
-                    className="text-primary hover:text-blue-800"
-                  >
-                    +30 6940970111
-                  </a>
-                </div>
-              </div>
               
               <div className="bg-white rounded-xl shadow-md p-6 flex items-start">
                 <div className="w-10 h-10 rounded-full bg-blue-100 flex items-center justify-center mr-4 flex-shrink-0">
